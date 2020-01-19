0edadedb10a8b12dfdd73ea1d2ef305b5422ece4
Sheldon300018918 <43228306+Sheldon300018918@users.noreply.github.com>
2020-01-19 23:28:20 +0900

    Add files via upload

--- candev project.py
+++ candev project.py
@@ -24,7 +24,7 @@ def make_form(numbers_1,numbers_2,times):
     
     canvas_tk.create_rectangle(0,0,200,50)#title block
     canvas_tk.create_text(30,25,text="time")
-    canvas_tk.create_text(150,25,text="ALberta")
+    canvas_tk.create_text(150,25,text="Alberta")
     canvas_tk.create_text(250,25,text="Nova Scotia")
     
     for position in range(int((len(numbers_1)*50)/50)):
@@ -56,6 +56,8 @@ def make_graph(numbers_1,numbers_2):
     canvas_tk = Tk()
     canvas_tk = Canvas(canvas_tk,width = 500,height = 500)
     canvas_tk.pack()
+    canvas_tk.create_line(0,250,500,250,fill = "red")#x-axis
+    canvas_tk.create_line(0,0,5,500,fill = "red")#y-axis
     
     for position in range(len(numbers_1)):  #draw line 1 part
         next_x = start_x+unit_x*position  #x keep increasing
